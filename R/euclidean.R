f2f79f66b9ed7216fd17374d0beecf6c0ecb0e80
Yemi Morrison <morrisonolayemi@gmail.com>
2023-09-17 12:20:10 +0200

    Updated the euclidean code

--- R/euclidean.R
+++ R/euclidean.R
@@ -5,20 +5,15 @@ euclidean <- function(x, y) {
     stop("Invalid input. Variables must be a numeric")
   } 
   
-  #Determine which of the numbers is larger
-  max_int <- max(c(x,y))
-  min_int <- min(c(x,y))
-  
-  #find the difference between the two integers
-  diff_int <- max_int - min_int
-
   #find the gcd
-  gcd <- 1
-  for (i in 1:max_int) {
-    if((x%% i) == 0 && (y %% i) == 0 && (diff_int %% i) == 0)
-      gcd <- i
+  while(y != 0) {
+    remainder <- x %% y
+    x <- y
+    y <- remainder
   }
-  return(gcd)
+  
+  return(x)
 }
+
 
 
